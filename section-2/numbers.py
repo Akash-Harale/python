836015e7f9e675be7db7adc8e3febca4f22b1305
Akash <akash.v.harale@gmail.com>
2025-02-15 17:37:50 +0530

    finished : lecture no. 16 - numbers and float

--- section-2/numbers.py
+++ section-2/numbers.py
@@ -18,4 +18,18 @@
 # final= int(age)+1
 # print(final)  -> 30
 
+# ----------------------------------------------------
+
+# price="36.2"
+
+# final= price+1
+# print(final) -> TypeError: can only concatenate str (not "int") to str
+
+# ----------------------------------
+
+# price=36.1
+
+# final = float(price)+1
+# print(final) -> 37.1
+
 
