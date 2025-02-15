795860f4a08290ba17c9dad2589e0952fa5250f9
Akash <akash.v.harale@gmail.com>
2025-02-15 17:31:26 +0530

    playin the numbers dataType

---
+++ section-2/numbers.py
@@ -0,0 +1,21 @@
+# age=30
+
+# final= age+1
+# print(final)  -> 31
+
+# --------------------------------------------
+
+# age="29"
+
+# final = age+1
+# print(final) ->   final = age+1
+#                   TypeError: can only concatenate str (not "int") to str
+
+# -------------------------------------------------------
+
+# age="29"
+
+# final= int(age)+1
+# print(final)  -> 30
+
+
