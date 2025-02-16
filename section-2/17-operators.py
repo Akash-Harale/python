ff075f20945e154ef79e33104223e5a82bd24871
Akash <akash.v.harale@gmail.com>
2025-02-16 10:55:31 +0530

    finished : lecture no. 17 - operators

---
+++ section-2/17-operators.py
@@ -0,0 +1,21 @@
+# addition --> also works with string ( concatination )
+print(5+6)
+
+# substraction
+print(6-8)
+
+# mulitpy --> also works with string ( to repeat the things.)
+print(8*9)
+print("hello"*2) # hellohello
+
+# dobule mulitpy
+print(5**2) # this will be 5 to the power 2. => 25
+
+# divide
+print(8/6)  # this will give float number i.e. actual answer
+
+# divide
+print(8//6) # this will give integer number. cut out numbers after decimal
+
+# modulus
+print(5%2) # this given the remainder => 1
